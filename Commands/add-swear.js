2bc7d62a103846cd9725fef96165b07a5612c3bc
only-the-wolf <106910864+only-the-wolf@users.noreply.github.com>
2022-06-05 14:11:16 +0300

    Create add-swear.js

---
+++ Commands/add-swear.js
@@ -0,0 +1,65 @@
+const Discord = require('discord.js');
+require('discord-reply');
+const Database = require('st.db')
+const db5 = new Database({path:'swears'})
+const client = new Discord.Client()
+
+module.exports = {
+
+name: "add-swear",//Command
+
+
+run: async(client, wolf) => {
+
+  try {
+
+let embed0 = new Discord.MessageEmbed()
+
+    .setTitle("**Missing Permissions**")
+    .setColor("RED")
+    .setDescription(`**❌ - You Don't Have \`OWNERSHIP\` Permission Use This Command**`)
+
+
+if(wolf.author.id !== wolf.guild.ownerID) return wolf.lineReply(embed0)
+
+let i = wolf.content.split(' ')[1]
+
+let embed00 = new Discord.MessageEmbed()
+
+    .setTitle("**Empty Message**")
+    .setColor("RED")
+    .setDescription(`**❌ - Write The Swear .**`)
+
+
+if(!i) return wolf.lineReply(embed00)
+
+if(db5.has({key: `swears_${wolf.guild.id}`}) == true){
+ let swears = db5.get({key: `swears_${wolf.guild.id}`})
+if(swears.includes(`swear_${i}`)) return wolf.lineReply(`**❌ - \`${i}\` Is Already In Swears List .**`)
+}
+
+let embed = new Discord.MessageEmbed()
+
+.setTitle(`**Swears List**`)
+
+.setColor("RANDOM")
+
+.setDescription(`**Done Add \`${i}\` To Swears List ✅ .**`)
+
+.setFooter(wolf.author.username,wolf.author.displayAvatarURL({dynamic: true}))
+
+wolf.lineReply(embed)
+
+db5.push({
+        key: `swears_${wolf.guild.id}`,
+        value: `swear_${i}`,
+      })
+
+
+
+  }catch(err){
+
+    return;
+  }
+},
+}
